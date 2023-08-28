36ffd9ee2b95610bad67d98cffa888275ff241cb
Aliya Najeeb <aliyanajeeb23@gmail.com>
2023-08-28 13:58:02 +0530

    admin dashboard service file

--- src/service/AdminService.js
+++ src/service/AdminService.js
@@ -5,25 +5,42 @@
 
 import axios from 'axios' ;
 
-const CUSTOMERS_REST_API_URL='http://localhost:8090/prismbank/account/';
+const CUSTOMERS_REST_API_URL='http://localhost:8090/prismbank/';
 
 class AdminService{
 
    static getCustomers(){
-        return axios.get(CUSTOMERS_REST_API_URL+"customers");
+        return axios.get(CUSTOMERS_REST_API_URL+"account/customers");
     }
 
     static viewCustomer(accountNumber){
-        return axios.get(CUSTOMERS_REST_API_URL+"customer/"+accountNumber);
+        return axios.get(CUSTOMERS_REST_API_URL+"account/customer/"+accountNumber);
     }
 
     static approveRequest(customer,accountNumber){
-        return axios.put(CUSTOMERS_REST_API_URL+"approve/"+accountNumber,customer);
+        return axios.put(CUSTOMERS_REST_API_URL+"account/approve/"+accountNumber,customer);
     }
 
     static deleteRequest(accountNumber){
-        return axios.delete(CUSTOMERS_REST_API_URL+"customer/"+accountNumber);
+        return axios.delete(CUSTOMERS_REST_API_URL+"account/customer/"+accountNumber);
     }
+
+    static depositCash(toAccount, amount) {
+        const data = {
+          accountNumber: toAccount,
+          amount: amount
+        };
+        return axios.post(CUSTOMERS_REST_API_URL+"transaction/cashdeposit", data);
+      }
+    
+      static withdrawCash(fromAccount, amount) {
+        const data = {
+          accountNumber: fromAccount,
+          amount: amount
+        };
+        return axios.post(CUSTOMERS_REST_API_URL+"transaction/cashwithdraw", data);
+      }
+    
 
 }
 
